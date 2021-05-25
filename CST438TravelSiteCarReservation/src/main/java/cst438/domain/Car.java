3eaad80016d640d6dc54abd410ce04a2ae8ff9ad
edgarleos <edgarleos@localhost>
2021-05-25 09:04:40 -0700

    Database change
    Database update to include city, state, quantity. Updated Car class and
    car_info accordingly.

--- CST438TravelSiteCarReservation/src/main/java/cst438/domain/Car.java
+++ CST438TravelSiteCarReservation/src/main/java/cst438/domain/Car.java
@@ -19,11 +19,14 @@ public class Car {
 	private String carClass;
 	private String fuelType;
 	private double rentalPrice;
+	private String state;
+	private String city;
+	private int quantity;
 	
 	public Car() {}
 
 	public Car(int id, String model, String make, int year, String trany, double cityMPG, double highwayMPG,
-			String carClass, String fuelType, double rentalPrice) {
+			String carClass, String fuelType, double rentalPrice, String state, String city, int quantity) {
 		super();
 		this.id = id;
 		this.model = model;
@@ -35,6 +38,9 @@ public class Car {
 		this.carClass = carClass;
 		this.fuelType = fuelType;
 		this.rentalPrice = rentalPrice;
+		this.state = state;
+		this.city = city;
+		this.quantity = quantity;
 	}
 
 	public int getId() {
@@ -117,6 +123,122 @@ public class Car {
 		this.rentalPrice = rentalPrice;
 	}
 
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", model=" + model + ", make=" + make + ", year=" + year + ", trany=" + trany
+				+ ", cityMPG=" + cityMPG + ", highwayMPG=" + highwayMPG + ", carClass=" + carClass + ", fuelType="
+				+ fuelType + ", rentalPrice=" + rentalPrice + ", state=" + state + ", city=" + city + ", quantity="
+				+ quantity + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((carClass == null) ? 0 : carClass.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(cityMPG);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((fuelType == null) ? 0 : fuelType.hashCode());
+		temp = Double.doubleToLongBits(highwayMPG);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + id;
+		result = prime * result + ((make == null) ? 0 : make.hashCode());
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + quantity;
+		temp = Double.doubleToLongBits(rentalPrice);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((trany == null) ? 0 : trany.hashCode());
+		result = prime * result + year;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (carClass == null) {
+			if (other.carClass != null)
+				return false;
+		} else if (!carClass.equals(other.carClass))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (Double.doubleToLongBits(cityMPG) != Double.doubleToLongBits(other.cityMPG))
+			return false;
+		if (fuelType == null) {
+			if (other.fuelType != null)
+				return false;
+		} else if (!fuelType.equals(other.fuelType))
+			return false;
+		if (Double.doubleToLongBits(highwayMPG) != Double.doubleToLongBits(other.highwayMPG))
+			return false;
+		if (id != other.id)
+			return false;
+		if (make == null) {
+			if (other.make != null)
+				return false;
+		} else if (!make.equals(other.make))
+			return false;
+		if (model == null) {
+			if (other.model != null)
+				return false;
+		} else if (!model.equals(other.model))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		if (Double.doubleToLongBits(rentalPrice) != Double.doubleToLongBits(other.rentalPrice))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (trany == null) {
+			if (other.trany != null)
+				return false;
+		} else if (!trany.equals(other.trany))
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
+	}
+	
+	
 
 	
 	
